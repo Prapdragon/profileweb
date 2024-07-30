c45babcbfbee76fa9a2dff8fcae1fdc2567a1543
Prapdragon <139678437+Prapdragon@users.noreply.github.com>
2024-07-30 22:21:10 +0700

    Add files via upload

---
+++ profile.blade.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <title>Open Profile</title>
+    <style>
+        h1 {
+            text-align: center;
+        }
+        .card{
+            max-width: 960px;
+            height: 500px;
+            margin: 0 auto;
+            /* border: 1px solid red; */
+            /* padding: 2rem;   */
+            display: flex;
+            justify-content: center;
+            align-items: center
+        }
+       .container{
+            align-items: flex-end;
+            justify-content: center;
+            display: flex;
+            max-width: 960px;
+            height: 100px;
+            /* border: 1px solid red; */
+            margin: 0 auto;
+            
+        }
+        /* body {
+        background-image: url(backgroundver2-2.jpg);
+        } */
+        /* body{
+        background-image: url(back.jpg);
+        } */
+        /* body{
+        background-image: url(backgroundver2.jpg);
+        } */
+        /* body{
+        background-image: url(backgroundwebb.png);
+        } */
+        /* body{
+        background-image: url(backgroundver2-3.jpg);
+        } */
+        /* body{
+        background-image: url(backgroundver2-4.jpg);
+        } */
+        body{
+        background-image: url(backgroundver2-5.jpg);
+        }
+        #left-head{
+            display: flex;
+            left: 0 auto;
+            max-width: 960px;
+        }
+    </style>
+</head>
+<body>
+    <div id="left-head" >
+        <h1 class="text-white">  Name Nopphadon Ladadok</h1>
+    </div>
+    <div>
+        <h1 class="text-info">Welcome to my E-Profile</h1>
+    </div>
+    <div class="card" style="width: 20rem;">
+        <div class="card_img">
+            <div class="card-img-warpper">
+                <img src="photo.png" class="card-img-warpper" style="max-width: 100%; height: auto;">
+            </div>
+        </div>
+    </div>
+    <div class="container">
+            <button class="btn btn-primary">
+                <h1 class="text-white">Into My Profile</h1>
+            </button>
+        </div>
+    <div class="container">
+        <h3 class="text-white">Made by Prapdragon</h3>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz4fnFO9oIkYo3FCPxVqC6G8jF5p1zHgglQtW7tvT5C6MQ6DRaL1+FCI/m" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.min.js" integrity="sha384-cA2pYvWvGe3Ug3oD0HBhAhgjbE0C0RBz8B9c3woJ3sIS/2VTT5wiXYk4V8y/NQ4s" crossorigin="anonymous"></script>
+</body>
+</html>
